bdd777e760f765746982c40517855d46db861199
xiucongysyx <xiucongysyx@gmail.com>
2023-06-24 16:52:40 +0800

    Exercise 2.2

---
+++ marco.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#marco函数，将当前文件夹的目录保存到环境变量MARCO_DIR
+marco(){
+    if [ -z "$1" ] || [ "$1" = "-y" ]; then
+            export MARCO_DIR=$(pwd)  #参数为-y就保存目录
+            echo "DIR has been saved"
+    elif [ "$1" = "-n" ]; then 
+            unset MARCO_DIR          #参数为-n就清楚保存的目录
+            echo "DIR has been cleared"
+    else
+            echo "Invalid argument!"
+    fi
+}
+polo(){
+    if [ -n "$MARCO_DIR" ]; then #if的-n 是检查字符串 MARCO_DIR 是否为空的
+        cd $MARCO_DIR
+    else
+        echo "The MARCO_DIR is empty!"
+    fi
+}
